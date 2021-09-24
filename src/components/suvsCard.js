a53235695c1216885466a7194e220f6d6374630e
dialectrical <michael.camele@gmail.com>
2021-09-23 19:31:55 -0700

    feat: built suvsCard

---
+++ src/components/suvsCard.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { StyledCard } from "./styles/StyledCard";
+import { StyledButton } from "./styles/StyledButton";
+import icon from "../img/icon-suvs.svg";
+
+export const SedansCard = () => {
+  return (
+    <StyledCard className="suvsCard">
+      <div className="imgContainer">
+        <img src={icon} />
+      </div>
+      <div className="headerContainer">
+        <h2>Sedans</h2>
+      </div>
+      <div className="contentContainer">
+        <p>
+          Choose a sedan for its affordability and excellent fuel economy. Ideal
+          for cruising in the city or on your next road trip.
+        </p>
+      </div>
+      <div className="buttonContainer">
+        <StyledButton className="suvsButton"> Learn More</StyledButton>
+      </div>
+    </StyledCard>
+  );
+};
