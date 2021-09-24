2777b4e20aa64fcca140307a18c3733750af4192
dialectrical <michael.camele@gmail.com>
2021-09-23 19:01:28 -0700

    feat: added StyledCard

---
+++ src/components/styles/StyledCard.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const StyledCard = styled.section`
+  width: 307px;
+  height: 500px;
+  border-radius: 8px;
+  padding: 48px 47.5px 48px 47.5px;
+  .sedansCard {
+    background-color: #e2b625;
+  }
+  .suvsCard {
+    backgroundpcolor: #006971;
+  }
+  .luxuryCard {
+    background-color: #004140;
+  }
+`;
