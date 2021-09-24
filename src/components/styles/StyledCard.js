6bf143072bfd62b2a5d98af2f40994d0acfe9464
dialectrical <michael.camele@gmail.com>
2021-09-23 19:08:14 -0700

    feat: added .headerContainer to card

--- src/components/styles/StyledCard.js
+++ src/components/styles/StyledCard.js
@@ -5,6 +5,9 @@ export const StyledCard = styled.section`
   height: 500px;
   border-radius: 8px;
   padding: 48px 47.5px 48px 47.5px;
+  .headerContainer {
+    margin: 35px 0px 25px 48px;
+  }
   .sedansCard {
     background-color: #e2b625;
   }
